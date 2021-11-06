a26b17fefc0a8d4b251d1d1b88b11c8ac3918316
NatalliaSaverchenko <saverchenko.nn@gmail.com>
2021-11-07 00:40:15 +0300

    add description in readme and svg bookmark in cardItem component

--- src/components/CardItem/CardItem.js
+++ src/components/CardItem/CardItem.js
@@ -1,6 +1,7 @@
 import './cardItem.css'
+import { faBookmark } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 const CardItem = ({ id, name, date, city, genre, image }) => {
-    
   return (
     <div className="cardItem">
       <div
@@ -8,8 +9,14 @@ const CardItem = ({ id, name, date, city, genre, image }) => {
         style={{ backgroundImage: `url(${image})` }}
       >
         <div className="cardItemInfo">
-          <div className='cardItemDate'>
-            <span>{date.slice(0,2)}</span>
+          <div className="cardItemBookMark">
+            <div className="cardItemDate">
+              <span>{date.slice(0, 2)}</span>
+            </div>
+            <div className='bookMark'>
+            <FontAwesomeIcon style={{fontSize: '18px'}} icon={faBookmark} />
+            </div>
+            
           </div>
           <div>
             <span>{name}</span>
